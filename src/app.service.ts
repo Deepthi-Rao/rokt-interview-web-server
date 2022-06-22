635315acde26bd30a9407a109d3b8d72a7b3c8a3
Deepthi Rao <deepthirao@Deepthis-MacBook-Pro.local>
2022-06-22 15:38:25 +0200

    final product

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {
+    
+  }
+  getAllImages(apiString: string): Observable<any> {
+    const url = apiString ? apiString : 'https://api.pexels.com/v1/curated?page=1&per_page=10';
+    let apiKey = '563492ad6f91700001000001e37a468ccc244367a5dae071a392cd00';
+    return this.httpService.get(url, {headers: {Authorization: apiKey}});
+
   }
 }
